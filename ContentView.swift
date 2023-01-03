5eab69cb5d4096174ac18b19974e45567afe6807
Dominik <domkwi18@gmail.com>
2023-01-03 22:47:44 +0100

    adding navigation link

--- ContentView.swift
+++ ContentView.swift
@@ -28,86 +28,89 @@ struct ContentView: View {
     
     
     var body: some View {
-        VStack {
-            Text("Put the Bull's Eys as close as you can to: \(randomValue)")
-            Slider(
-                value: $sliderValue,
-                in: 1...100,
-                minimumValueLabel: Text("1"),
-                maximumValueLabel: Text("100"),
-                label: {
-                    Text("random number")
-                }
-            )
-            
-            Button("Hit me!") {
-                print("Hit")
-                showingAlert = true
+        NavigationView {
+            VStack {
+                Text("Put the Bull's Eys as close as you can to: \(randomValue)")
+                Slider(
+                    value: $sliderValue,
+                    in: 1...100,
+                    minimumValueLabel: Text("1"),
+                    maximumValueLabel: Text("100"),
+                    label: {
+                        Text("random number")
+                    }
+                )
                 
-                
-                let roundedSliderValue:Int = Int(sliderValue)
-                
-                if randomValue > roundedSliderValue {
-                    LoseGame()
-                    difference = randomValue - roundedSliderValue
-                    print("Random value is now bigger than rounded slider value")
+                Button("Hit me!") {
+                    print("Hit")
+                    showingAlert = true
                     
-                } else if randomValue < roundedSliderValue {
-                    LoseGame()
-                    difference = randomValue - roundedSliderValue
-                    print("Random Value is now lower that rounded slider value")
                     
-                } else {
-                    WinGame()
-                    difference = 0
-                    print("you win")
+                    let roundedSliderValue:Int = Int(sliderValue)
+                    
+                    if randomValue > roundedSliderValue {
+                        LoseGame()
+                        difference = randomValue - roundedSliderValue
+                        print("Random value is now bigger than rounded slider value")
+                        
+                    } else if randomValue < roundedSliderValue {
+                        LoseGame()
+                        difference = randomValue - roundedSliderValue
+                        print("Random Value is now lower that rounded slider value")
+                        
+                    } else {
+                        WinGame()
+                        difference = 0
+                        print("you win")
+                    }
+                    
                 }
-                
-            }
-            .alert(isPresented: $showingAlert) {
-                Alert(
-                    title: Text("Hello World!"),
-                    message: Text("The value of the slider is now: \(randomValue)" +
-                                 "\nThe target value is: \(sliderValue) " +
-                                 "\nThe diffrence is: \(difference)"),
-                    dismissButton: .default(Text("Ok"), action: {
-                        randomValue = Int.random(in: 1...100);
-                        sliderValue = 50
-                    })
-                    )
-            }
-            
-            HStack(alignment: .bottom, spacing: 150){
-                
-                Button("Restart") {
-                    showingAlert2 = true
-                    print("clicked")
-                }
-                .alert(isPresented: $showingAlert2) {
+                .alert(isPresented: $showingAlert) {
                     Alert(
-                        title: Text("Reset Game?"),
-                        message: Text("You want to reset the game?"),
-                        primaryButton: .default(
-                            Text("No")
-                        ),
-                        secondaryButton: .destructive(
-                            Text("Yes"),
-                            action: {
-                                LoseGame();
-                                sliderValue = 50
-                            }
-                        )
+                        title: Text("Hello World!"),
+                        message: Text("The value of the slider is now: \(randomValue)" +
+                                      "\nThe target value is: \(sliderValue) " +
+                                      "\nThe diffrence is: \(difference)"),
+                        dismissButton: .default(Text("Ok"), action: {
+                            randomValue = Int.random(in: 1...100);
+                            sliderValue = 50
+                        })
                     )
                 }
                 
-                Text("Score: \(score)")
-                Text("Round: \(round)")
-                
-                Button("i") {
-                    print("information")
+                HStack(alignment: .bottom, spacing: 150){
+                    
+                    Button("Restart") {
+                        showingAlert2 = true
+                        print("clicked")
+                    }
+                    .alert(isPresented: $showingAlert2) {
+                        Alert(
+                            title: Text("Reset Game?"),
+                            message: Text("You want to reset the game?"),
+                            primaryButton: .default(
+                                Text("No")
+                            ),
+                            secondaryButton: .destructive(
+                                Text("Yes"),
+                                action: {
+                                    LoseGame();
+                                    sliderValue = 50
+                                    randomValue = Int.random(in: 1...100)
+                                }
+                            )
+                        )
+                    }
+                    
+                    Text("Score: \(score)")
+                    Text("Round: \(round)")
+                    
+                    NavigationLink(destination: InformationView()) {
+                        Text("i")
+                    }
                 }
+                .padding(.top, 100.0)
             }
-            .padding(.top, 100.0)
         }
     }
 }
